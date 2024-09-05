929a604149039baf93b8b66171685c5e33b5fa33
Nurswizz <nur646132@gmail.com>
2024-09-05 20:32:03 +0500

    added some kind of design

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const express = require('express');
 const getData = require('./fetch');
 const app = express();
 app.set('view engine', 'ejs');
-const port = process.env.PORT;
+app.use(express.static('public'));
+const port = process.env.PORT || 3000;
 
 app.get('/', async (req, res) =>{
     const food = req.query.food;
     if(food === undefined) return res.render('index', {c :[], err : ""});
     const r = await getData(food);
-    console.log(r);
     if (r.length === 0) return res.render('index', {c: [], err: "There is no such kind of food" });
     return res.render('index', {c: r, err: ""});
     
